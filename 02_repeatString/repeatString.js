3c6558d678cb71d5525da939f507f679d543f898
irao12 <ivanrao@outlook.com>
2022-06-17 12:37:21 -0400

    task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, reps) {
+    let res = "";
+    if (reps < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < reps; i++){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
