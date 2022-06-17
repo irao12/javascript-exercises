4dc6130a1eb2c4c13d7df37b6983fa66dd950028
irao12 <ivanrao@outlook.com>
2022-06-17 12:38:10 -0400

    task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let result = 0;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if (num2 < num1){
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    for (let i = num1; i <= num2; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
